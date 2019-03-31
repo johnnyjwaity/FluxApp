1b7527fd42a922bb130f155774a02003bb4f8a11
Johnny Waity <johnnywaity@gmail.com>
2019-03-31 02:29:36 -0500

    Initial Commit

---
+++ Flux/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flux
+//
+//  Created by Johnny Waity on 3/31/19.
+//  Copyright © 2019 Johnny Waity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
